36cca3aa2fb8f52fd2146e0c27a007ac266982e7
luxiaojia <490423467@qq.com>
2017-04-22 08:26:13 +0800

    final version

--- app/controller.js
+++ app/controller.js
@@ -15,4 +15,12 @@ angular.module("myApp", ['ngRoute'])
 	];
 	$scope.show = false;
 	$scope.showMore = ["设置栏目", "更多设置", "分割线"];
+	$scope.str = "";
+	$scope.info = [
+		{name:"悠懒瑶瑶", info:"欢迎关注", avatar:"images/a5.jpg"},
+		{name:"iG_Lucio", info:"hi(=′ー`) ,谢谢你的关注与支持！消息我有时间都会回复，我们会继续努力的！", avatar:"images/a6.jpg"},
+		{name:"帅气可爱无敌迷人的妈妈的大", info:"谢谢你关注了我！！你真可爱！！", avatar:"images/a7.jpg"},
+		{name:"小绝Drug补给站", info:"感谢关注(◍ ´꒳` ◍)恭喜你发现了一只小绝drug中毒重症患者！", avatar:"images/a8.jpg"},
+		{name:"小绝QAQ", info:"感谢关注！！有什么想说的可以直接告诉我！！", avatar:"images/a9.jpg"}
+	];
 });
